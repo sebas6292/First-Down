8f9b43d1cf5b09c17f19b54ecf6afdc5d8371b4e
andres_me <amedra13@gmail.com>
2022-10-19 21:37:50 -0700

    skeleton placeholder for front display

--- first-down/src/App.js
+++ first-down/src/App.js
@@ -2,11 +2,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Header from './LandingPage/header.jsx';
+import Display from './LandingPage/display.jsx';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header /> 
+      <Display/>
     </div>
   );
 }
