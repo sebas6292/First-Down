dc3eb76404db30f1fb97ef831ce044d9d4772cf6
Sebastian Leal <sebas6292@gmail.com>
2022-10-19 19:36:33 -0700

    accepted the changes

--- first-down/src/App.js
+++ first-down/src/App.js
@@ -5,12 +5,7 @@ import Header from './header.tsx';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <h1>Hello</h1>
-=======
-      <Header message='This is a test' /> 
-      <h1 style={{textAlign: 'center'}}>Starting Fresh</h1>
->>>>>>> 4d04655799e14eba8735672559388602d6924b88
     </div>
   );
 }
